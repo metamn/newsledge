110cdb9da6d8ee3ee56c06cdc76523917ac7eb27
rubycs <rubycs@clair.ro>
2011-06-16 17:40:37 +0300

    Rails 3.1.rc1 added

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Newsledge::Application.config.secret_token = '6620962ffb8493f6be2c76880a4943592ae3a77fa8fb4abb1315997544ecda30f41297256ccd28eca70a5e90b486d2429d98df0bcf7d828093238f589e761bdb'
